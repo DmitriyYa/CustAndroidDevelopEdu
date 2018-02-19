88ef7ea13d36ea4256d8255245bd94fa0a7dd1c4
Dima Home <dmitriy.m.yakovlev@gmail.com>
2018-02-19 20:24:06 +0600

    Initial commit

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+
+     private Point a;
+     private Point b;
+     private Point c;
+
+    public Triangle(Point a, Point b, Point c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+
+    public boolean isRectangular(Triangle triangle){
+
+
+        return false;
+    }
+}
